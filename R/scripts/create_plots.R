8534a62f717396406b3e4709a59a0f564d8e55d8
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-04-13 14:36:59 -0500

    created first bar chart

--- R/scripts/create_plots.R
+++ R/scripts/create_plots.R
@@ -3,8 +3,50 @@
 #
 # Cecile Murray
 #===============================================================================#
+
 library(here)
 source("scripts/setup.R")
+
+setwd(MAIN_DIR)
+tcrlus <- readstata13::read.dta13("output/FINAL_merged_TCRLUS_census_buildperm.dta")
+setwd(here())
+
+chart_theme <- function(...) {
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_line(color = "gray75",
+                                        size = rel(0.75),
+                                        linetype = "dotted"),
+        text = element_text(family = "Lucida Grande")) +
+  theme(...)
+}
+
+#===============================================================================#
+# BAR CHART: SHARE OF LAND ZONED FOR MF VS. SF
+#===============================================================================#
+
+land_share <- data.frame(sf = table(tcrlus$lnd_sf),
+                mf = table(tcrlus$lnd_mf),
+                nr = table(tcrlus$lnd_nr)) %>% 
+  select(sf.Var1, contains("Freq")) %>% 
+  dplyr::rename(land_share = sf.Var1,
+                SF = sf.Freq,
+                MF = mf.Freq,
+                NR = nr.Freq) %>% 
+  filter(!land_share %in% c("Missing", "Varies, inconsistent")) %>% 
+  gather(key = "zone", value = "ct", SF:NR) %>% 
+  filter(zone != "NR")
+
+ggplot(land_share, aes(x = land_share, y = ct, group = zone, fill = zone)) +
+  geom_col(position = "dodge") +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  labs(title = "Most municipalities reserve most of their land for single-family units",
+       subtitle = "Share of land zoned to allow multifamily vs. single-family construction",
+       x = "Share of land zoned", y = "Number of municipalities",
+       caption = "Source: Terner Center Land Use Survey") +
+  chart_theme(axis.ticks.x = element_blank(),
+        legend.position = c(0.8, 0.85),
+        legend.title = element_blank())
+
 
 #===============================================================================#
 # SMALL MULTIPLES/SCATTER: PREDICTED CONSTRUCTION IN CA
@@ -26,3 +68,5 @@ ggplot(scatter_data, aes(x = rrent10, y = mf_new, color = metro_name)) +
   theme(legend.position = c(0.8, 0.15),
         text = element_text(family='Lucida Grande'))
 ggsave('plots/fig4_small_multiples.png', width = 11, height = 8.5)
+
+
