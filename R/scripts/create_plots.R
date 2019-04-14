fca3b64ad60c2c40f0f7469872ec7b86ca1d6416
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-04-14 11:00:45 -0500

    first round of figures done

--- R/scripts/create_plots.R
+++ R/scripts/create_plots.R
@@ -1,5 +1,5 @@
 #===============================================================================#
-# CREATE TRCLUS CHARTS
+# CREATE TCRLUS CHARTS
 #
 # Cecile Murray
 #===============================================================================#
@@ -10,7 +10,6 @@ source("scripts/setup.R")
 # bring in final dataset
 setwd(MAIN_DIR)
 tcrlus <- haven::read_dta("output/FINAL_merged_TCRLUS_census_buildperm.dta")
-setwd(here())
 
 #===============================================================================#
 # THEMING /AESTHETICS
@@ -20,6 +19,7 @@ setwd(here())
 terner_blue <- "#4F758B" # c(79, 117, 139)
 terner_gold <- "#F1A900" # c(241, 169, 0)
 
+# define theme
 chart_theme <- function(...) {
   theme(panel.background = element_blank(),
         panel.grid.major = element_line(color = "gray75",
@@ -34,6 +34,7 @@ chart_theme <- function(...) {
 # BAR CHART: SHARE OF LAND ZONED FOR MF VS. SF
 #===============================================================================#
 
+# create frequency tables, filter out unwanted categories
 land_share <- data.frame(sf = table(haven::as_factor(tcrlus$lnd_sf)),
                 mf = table(haven::as_factor(tcrlus$lnd_mf)),
                 nr = table(haven::as_factor(tcrlus$lnd_nr))) %>% 
@@ -42,13 +43,15 @@ land_share <- data.frame(sf = table(haven::as_factor(tcrlus$lnd_sf)),
                 SF = sf.Freq,
                 MF = mf.Freq,
                 NR = nr.Freq) %>% 
-  filter(!land_share %in% c("Missing", "Varies, inconsistent")) %>% 
+  filter(!land_share %in% c("Missing", "Varies, inconsistent", "N/A, none")) %>% 
   gather(key = "zone", value = "ct", SF:NR) %>% 
   filter(zone != "NR")
 
+# create plot
 ggplot(land_share, aes(x = land_share, y = ct, group = zone, fill = zone)) +
   geom_col(position = "dodge") +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  scale_fill_discrete(labels = c("Multifamily", "Single-family")) +
   labs(title = "Most municipalities reserve most of their land for single-family units",
        subtitle = "Share of land zoned to allow multifamily vs. single-family construction",
        x = "Share of land zoned", y = "Number of municipalities",
@@ -56,6 +59,7 @@ ggplot(land_share, aes(x = land_share, y = ct, group = zone, fill = zone)) +
   chart_theme(axis.ticks.x = element_blank(),
         legend.position = c(0.8, 0.85),
         legend.title = element_blank())
+# ggsave("plots/fig2_land_share.png", width = 7, height = 5, units = c("in"))
 
 #===============================================================================#
 # BAR CHART: MAX BUILDING HEIGHT
@@ -79,12 +83,15 @@ max_height <- tcrlus %>% select(contains("heightlimit")) %>%
 
 ggplot(max_height, aes(x = limit, y = ct, group = zone, fill = zone)) +
   geom_col(position = "dodge") +
+  scale_x_continuous(breaks = seq(0, 50, 5)) +
+  scale_fill_discrete(labels = c("Multifamily", "Single-family")) +
   labs(title = "Most respondents restrict building heights to four stories or fewer",
        subtitle = "Adjusted building height limits",
        x = "Height limit", y = "Number of municipalities",
        caption = "Source: Terner Center Land Use Survey") +
   chart_theme(legend.title = element_blank(),
               legend.position = c(0.8, 0.9))
+# ggsave("plots/fig3_bld_hgt.png", width = 7, height = 5, units = c("in"))
 
 #===============================================================================#
 # HISTOGRAM: DU / ACRE
@@ -97,11 +104,14 @@ density <- tcrlus %>% select(contains("maxdensity")) %>%
   filter(max_density > 0, zone == "MF")
 
 ggplot(density, aes(x = max_density)) +
-  geom_histogram(fill = terner_blue, color = "white") +
+  geom_histogram(binwidth = 5, fill = terner_blue, color = "white") +
+  scale_x_continuous(breaks = seq(0, 200, 25)) +
   labs(title = "Many municipalities restruct multifamily density",
+       x = "Maximum dwelling units per acre", y = "Number of municipalities",
        subtitle = "Maximum multifamily dwelling units per acre",
        source = "Terner Center Land Use Survey") +
   chart_theme(legend.title = element_blank())
+# ggsave("plots/max_du_acre_histogram.png", width = 7, height = 5, units = c("in"))
 
 #===============================================================================#
 # BAR CHART: APPROVAL TIMES
@@ -120,14 +130,15 @@ approvals <- tcrlus %>% select(x5_units_units, apt_mfconsistent) %>%
 
 ggplot(approvals, aes(x = apt_mfconsistent, y = ct, group = permit_level,
                       fill = permit_level)) +
-  scale_x_discrete(name = "Permit Level",
-                   labels = function(x) str_wrap(x, width = 10)) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  scale_fill_discrete(name = "Permits issued") +
   geom_col(position = "dodge") +
   labs(title = "Municipalities that didn't permit many units reported \nshorter approval times",
        subtitle = "Approval time for multifamily projects consistent with general zoning",
        x = "Approval time", y = "Number of municipalities",
        source = "Terner Center Land Use Survey") +
   chart_theme(legend.position = c(0.8, 0.8))
+ggsave("plots/fig4_approval_times.png", width = 7, height = 5, units = c("in"))
 
 #===============================================================================#
 # BAR CHART: APPLICATIONS
@@ -145,14 +156,15 @@ apps <- tcrlus %>% select(x5_units_units, apl_mf5to19) %>%
 
 ggplot(apps, aes(x = apl_mf5to19, y = ct, group = permit_level,
                       fill = permit_level)) +
-  scale_x_discrete(name = "Permit Level",
-                   labels = function(x) str_wrap(x, width = 10)) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  scale_fill_discrete(name = "Permits issued") +
   geom_col(position = "dodge") +
   labs(title = "Municipalities that didn't permit many units reported \nless frequent applications",
        subtitle = "Frequency of approvals for multifamily projects between 5 and 19 units",
        x = "Application frequency", y = "Number of municipalities",
        source = "Terner Center Land Use Survey") +
   chart_theme(legend.position = c(0.8, 0.8))
+ggsave("plots/fig8_applications.png", width = 7, height = 5, units = c("in"))
 
 
 #===============================================================================#
